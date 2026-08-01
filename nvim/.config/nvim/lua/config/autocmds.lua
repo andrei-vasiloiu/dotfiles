7399a41e72e6ce05fc51e9491354e9dfc285444c
Andrei Vasiloiu <42448233+andrei-vasiloiu@users.noreply.github.com>
2026-08-01 09:33:49 -0500

    nvim: add filetype indentation rules

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -28,3 +28,25 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = group,
+  pattern = {
+    "css",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "jsonc",
+    "lua",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.tabstop = 2
+  end,
+})
