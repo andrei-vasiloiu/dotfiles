394e75aa80876a6dcb0beeb5821fdf1fec899d58
Andrei Vasiloiu <42448233+andrei-vasiloiu@users.noreply.github.com>
2026-08-01 09:37:28 -0500

    nvim: refine search and split ergonomics

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -24,3 +24,15 @@ map("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "Increase split height" })
 map("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Decrease split height" })
 map("n", "<C-Left>", "<cmd>vertical resize -4<CR>", { desc = "Decrease split width" })
 map("n", "<C-Right>", "<cmd>vertical resize +4<CR>", { desc = "Increase split width" })
+
+map("n", "n", "nzzzv", { desc = "Next search result centered" })
+map("n", "N", "Nzzzv", { desc = "Previous search result centered" })
+
+map("n", "J", "mzJ`z", { desc = "Join lines without moving cursor" })
+
+map("n", "<leader>sv", "<cmd>vsplit<CR>", { desc = "Vertical split" })
+map("n", "<leader>sh", "<cmd>split<CR>", { desc = "Horizontal split" })
+map("n", "<leader>se", "<C-w>=", { desc = "Equalize splits" })
+map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close split" }
+
+
