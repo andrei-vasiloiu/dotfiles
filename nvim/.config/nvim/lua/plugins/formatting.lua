bddfde82e6d2f179f7f425bf5e5007460bbe634b
Andrei Vasiloiu <42448233+andrei-vasiloiu@users.noreply.github.com>
2026-08-01 01:55:48 -0500

    nvim: expand project-local formatting

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -6,7 +6,16 @@ return {
 
     opts = {
       formatters_by_ft = {
-        python = { "ruff_format" },
+          python = { "ruff_format" },
+          javascript = { "prettier", stop_after_first = true },
+          javascriptreact = { "prettier", stop_after_first = true },
+          typescript = { "prettier", stop_after_first = true },
+          typescriptreact = { "prettier", stop_after_first = true },
+          json = { "prettier", stop_after_first = true },
+          jsonc = { "prettier", stop_after_first = true },
+          css = { "prettier", stop_after_first = true },
+          html = { "prettier", stop_after_first = true },
+          markdown = { "prettier", stop_after_first = true },
       },
 
       format_on_save = {
