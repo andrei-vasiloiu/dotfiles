80b226d97aa266e5651fa32301b5ee6455ff08a2
Andrei Vasiloiu <42448233+andrei-vasiloiu@users.noreply.github.com>
2026-08-01 01:49:38 -0500

    nvim: add WSL clipboard provider

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -35,4 +35,20 @@ opt.updatetime = 250
 opt.timeoutlen = 400
 opt.completeopt = { "menu", "menuone", "noselect" }
 
+local release = vim.uv.os_uname().release:lower()
+
+if release:find("microsoft", 1, true) then
+  vim.g.clipboard = {
+    name = "WSL clipboard",
+    copy = {
+      ["+"] = "/mnt/c/Windows/System32/clip.exe",
+      ["*"] = "/mnt/c/Windows/System32/clip.exe",
+    },
+    paste = {
+      ["+"] = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoLogo -NoProfile -Command '[Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace(\"`r\", \"\"))'",
+      ["*"] = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoLogo -NoProfile -Command '[Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace(\"`r\", \"\"))'",
+    },
+    cache_enabled = 0,
+  }
+end
 opt.clipboard = "unnamedplus"
