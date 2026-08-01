4d2b8508ce7e213d6e18145473125b69405c9eb5
Andrei Vasiloiu <42448233+andrei-vasiloiu@users.noreply.github.com>
2026-07-31 22:56:02 -0500

    nvim: add Python LSP and Ruff formatting

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -45,6 +45,23 @@ return {
           end, "Next diagnostic")
         end,
       })
-    end,
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+          group = group,
+          callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+              if client and client.name == "ruff" then
+                  client.server_capabilities.hoverProvider = false
+              end
+          end,
+      })
+
+      vim.lsp.enable({
+          "basedpyright",
+          "ruff",
+      })
+
+  end,
   },
-}
+  }
